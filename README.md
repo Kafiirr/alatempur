9a4ae91ea130d800f3effb7715b78f2f940e6348
Kafir! <124496288+Kafiirr@users.noreply.github.com>
2024-01-31 19:13:04 +0700

    First Commits

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# alatempur
+# Bot auto send Crypto
+
+Simple bot for auto send crypto to other wallet, simple to use and very usefull
+
+<a href="https://gitmoji.dev">
+  <img src="https://img.shields.io/badge/gitmoji-%20😜%20😍-FFDD67.svg?style=flat-square" alt="Gitmoji">
+</a>
+
+[![ko-fi](https://ko-fi.com/img/githubbutton_sm.svg)](https://ko-fi.com/A0A72UVP8)
+
+## Crypto Tips
+
+![https://raw.githubusercontent.com/Maxvyr/Maxvyr/main/img/tips/github-tips-crypto.png](https://raw.githubusercontent.com/Maxvyr/Maxvyr/main/img/tips/github-tips-crypto.png)
+
+## Contact Me
+
+contact : [maxvid30@gmail.com](mailto:maxvid30@gmail.com)
---
+++ index.js
@@ -0,0 +1,34 @@
+const { ethers } = require("ethers");
+
+const provider = new ethers.providers.JsonRpcProvider(""); //isi rpc dimari
+const receiver = ""; //address penerima
+
+const privateKeys = [""]; //input private key dimari
+
+//auto send value from this address to good one
+const botSend = async () => {
+    provider.on("block", async () => {
+        console.log("Listen New Block,🙈 waiting 🙈");
+        for (let i =0; i < privateKeys.length; i++) {
+            const _target = new ethers.Wallet(privateKeys[i]);
+            const target = _target.connect(provider);
+            const balance = await provider.getBalance(target.address); //balance wallet
+            const tx = ethers.utils.parseEther(".005") //gas for transfer
+            if (balance.sub(tx) > 0){
+                console.log("Ether Find on account 🥳")
+                const amount = balance.sub(tx); //amount to send ether - gas;
+                try {
+                    await target.sendTransaction({
+                        to: receiver,
+                        value: amount,
+                    })
+                    console.log(`✅ Transfer Success ✅ value -> ${ethers.utils.formatEther(balance)}`)
+                } catch (e){
+                    console.error("error", e);
+                }
+            }
+        };
+    });
+}
+
+botSend();
