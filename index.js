2922a3871b1b7c14bebefacff5da26b21e13f52d
Kafir! <124496288+Kafiirr@users.noreply.github.com>
2024-01-31 19:26:39 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,18 +5,18 @@ const receiver = ""; //address penerima
 
 const privateKeys = [""]; //input private key dimari
 
-//auto send value from this address to good one
+//auto send value dari addr ini ke addr penerima
 const botSend = async () => {
     provider.on("block", async () => {
         console.log("Listen New Block,🙈 waiting 🙈");
         for (let i =0; i < privateKeys.length; i++) {
             const _target = new ethers.Wallet(privateKeys[i]);
             const target = _target.connect(provider);
-            const balance = await provider.getBalance(target.address); //balance wallet
-            const tx = ethers.utils.parseEther(".005") //gas for transfer
+            const balance = await provider.getBalance(target.address); //balance addr
+            const tx = ethers.utils.parseEther(".005") //gas untuk transfer
             if (balance.sub(tx) > 0){
                 console.log("Ether Find on account 🥳")
-                const amount = balance.sub(tx); //amount to send ether - gas;
+                const amount = balance.sub(tx); //seberapa banyak untuk dikirim value ether - gas;
                 try {
                     await target.sendTransaction({
                         to: receiver,
